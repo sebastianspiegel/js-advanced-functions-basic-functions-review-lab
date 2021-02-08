9bcb11924e099e2159e4187c9ec68575086a1041
sebastianspiegel <sebastian.j.spiegel@gmail.com>
2021-02-08 10:22:51 -0500

    commit tree d0b335939093ad64c9a692bcf0be78211a8ef447

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Your code here
+
+function saturdayFun(activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`
+}
+
+let mondayWork = function(activity = "go to the office") {
+    return `This Monday, I will ${activity}.`
+}
+
+function wrapAdjective(flair = "*") {
+    return function(adj = "special"){
+        return `You are ${flair}${adj}${flair}!`
+    }
+}
+
+let Calculator = {
+    add: function(a,b){
+        return a + b
+    },
+    subtract: function(a,b){
+        return a - b
+    },
+    multiply: function(a,b){
+        return a * b
+    },
+    divide: function(a,b){
+        return a / b
+    }
+}
+
+function actionApplyer(int, array) {
+    let a = int 
+    for (let i = 0; i < array.length; i++){
+        a = array[i](a)
+    }
+    return a
+}
